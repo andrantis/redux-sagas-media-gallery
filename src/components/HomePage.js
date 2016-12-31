ced7bdf92b77ee416a55fc0432b002271fe3dbf8
Andres Antista <andresantista@gmail.com>
2016-12-31 11:56:43 -0800

    Wiring basic routes and initial layout of app

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const HomePage = () => {
+    return (
+        <div className="jumbotron center">
+            <h1 classNAme="lead">Welcome to Media Library built with React, Redux and Redux-Saga</h1>
+            <div>
+                <Link to="library">
+                    <button type="button" className="btn btn-lg btn-primary">Visit Library</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
